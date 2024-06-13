9b81b51b50fdd131a6ee02cd7928aaed2efec01d
Terranikon <slavo@mac.com>
2024-06-13 13:43:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app link
-  metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
+  domainSource: "https://5648f1fb-0df2-4baf-8f15-f8785056aae5.weweb-preview.io/", // Your WeWeb app link
+  metaDataEndpoint: "https://xzlq-1eat-f6yv.n7.xano.io/api:uksO2MFT/articles/get_single/{articles_id}", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
   patterns: {
-    dynamicPage: "/event/[^/]+"
+    dynamicPage: "/article/[^/]+"
   }
 };
